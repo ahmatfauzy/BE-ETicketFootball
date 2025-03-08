5e4a31e6b52ac42125931f93cc5621ea7df7d16c
ahmatfauzy <ahmatf703@gmail.com>
2025-03-09 01:38:29 +0700

    update cors

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(corsMiddleware);
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
@@ -29,12 +29,3 @@ app.use("/api/match", matchRoutes)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// Jika menggunakan Express.js
-// app.use(cors({
-//   origin: 'http://localhost:5173', // atau '*' untuk pengujian
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-
-// app.use(cors({ origin: 'http://localhost:5173' })); 
